dab943b45aa28c7a3610039da1f226afc92f8379
Princess Ailama O. Valmores <aceorpiano01@gmail.com>
2026-04-13 16:39:34 +0800

    edited supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://zunhmugudjmxocpdayut.supabase.co'
 const supabaseKey = 'sb_publishable_bQMuOILjhbfT3sDq63jq6A_dpBm6zOf'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabase)
