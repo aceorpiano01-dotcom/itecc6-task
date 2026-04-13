0671016e2b74f17899cd13f8eb42d9c8e6fe2c35
Princess Ailama O. Valmores <aceorpiano01@gmail.com>
2026-04-13 16:42:17 +0800

    Final changes

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ import HelloWorld from './components/HelloWorld.vue'
   <div>
     <h1>My Vue App</h1>
     <p>Name: Princess Ailama O. Valmores</p>
+    <p>Course: BS Information Technology</p>
+    <p>Year: 3rd Year</p>
     <p>Update 1</p>
   </div>
 </template>
